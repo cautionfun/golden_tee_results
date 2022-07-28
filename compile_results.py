95e8bc0cdb55159aeb9325ba31eea5bc22410800
cautionfun <94437013+cautionfun@users.noreply.github.com>
2022-07-28 01:06:10 -0400

    Add files via upload

---
+++ compile_results.py
@@ -0,0 +1,13 @@
+##compile results
+##MAKE SURE TO RUN THIS IN THE RELEVANT DIRECTORY
+##check working directory with print("Current working directory: {0}".format(os.getcwd()))
+import os
+import glob
+import pandas as pd
+##tell it to go through each file in the chosen directory and pick out every one with the csv file extension
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+##read and combine all relevant files into one variable
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+##spit that out into a csv file located in a folder to be read by filter_sort_results.py
+combined_csv.to_csv( "final/combined_csv.csv", index=False, encoding='utf-8-sig')
---
+++ filter_sort_results.py
@@ -0,0 +1,37 @@
+##filter results
+##loads in the compiled results
+results = pd.read_csv("final/combined_csv.csv")
+##filters for course and game mode and saves each row that returns TRUE as a variable
+##'98 Speed Runs
+sr_ah = results[(results["course"] == 'Arbor Hill') & (results["mode"] == 'Speed Run')]
+sr_bb = results[(results["course"] == 'Bayou Bend') & (results["mode"] == 'Speed Run')]
+sr_pg = results[(results["course"] == 'Palm Grove') & (results["mode"] == 'Speed Run')]
+##'99 Speed Runs
+sr_al = results[(results["course"] == 'Aspen Lake') & (results["mode"] == 'Speed Run')]
+sr_cc = results[(results["course"] == 'Coconut Cove') & (results["mode"] == 'Speed Run')]
+sr_rs = results[(results["course"] == 'Rancho Saguaro') & (results["mode"] == 'Speed Run')]
+##2k Speed Runs
+sr_sv = results[(results["course"] == 'Stone Valley') & (results["mode"] == 'Speed Run')]
+sr_sh = results[(results["course"] == 'Sea Haven') & (results["mode"] == 'Speed Run')]
+sr_cr = results[(results["course"] == 'Coyote Run') & (results["mode"] == 'Speed Run')]
+##Classic Speed Runs
+sr_ms = results[(results["course"] == 'Mountain Springs') & (results["mode"] == 'Speed Run')]
+sr_ac = results[(results["course"] == 'Anchor Cove') & (results["mode"] == 'Speed Run')]
+sr_sb = results[(results["course"] == 'Scorpian Bend') & (results["mode"] == 'Speed Run')]
+##now we send the filtered variables to their appropriate folders, each as their own csv file
+##'98
+sr_ah.to_csv("final/SpeedRun/98/ArborHill.csv", index=False, encoding='utf-8-sig')
+sr_bb.to_csv("final/SpeedRun/98/BayouBend.csv", index=False, encoding='utf-8-sig')
+sr_pg.to_csv("final/SpeedRun/98/PalmGrove.csv", index=False, encoding='utf-8-sig')
+##'99
+sr_al.to_csv("final/SpeedRun/99/AspenLake.csv", index=False, encoding='utf-8-sig')
+sr_cc.to_csv("final/SpeedRun/99/CoconutCove.csv", index=False, encoding='utf-8-sig')
+sr_rs.to_csv("final/SpeedRun/99/RanchoSaguaro.csv", index=False, encoding='utf-8-sig')
+##2k
+sr_sv.to_csv("final/SpeedRun/2k/StoneValley.csv", index=False, encoding='utf-8-sig')
+sr_sh.to_csv("final/SpeedRun/2k/SeaHaven.csv", index=False, encoding='utf-8-sig')
+sr_cr.to_csv("final/SpeedRun/2k/CoyoteRun.csv", index=False, encoding='utf-8-sig')
+##Classic
+sr_ms.to_csv("final/SpeedRun/Classic/MountainSprings.csv", index=False, encoding='utf-8-sig')
+sr_ac.to_csv("final/SpeedRun/Classic/AnchorCove.csv", index=False, encoding='utf-8-sig')
+sr_sb.to_csv("final/SpeedRun/Classic/ScorpianBend.csv", index=False, encoding='utf-8-sig')
